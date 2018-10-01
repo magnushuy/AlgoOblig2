3ea83ad608832875b26d9ea503615e6b6b24c176
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-10-01 17:34:47 +0200

    Oppgave1 wip

---
+++ src/Oblig/Main.java
@@ -0,0 +1,11 @@
+package Oblig;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Oppgave1
+        Liste<String> liste = new DobbeltLenketListe<>();
+        System.out.println(liste.antall() + " " + liste.tom());
+    }
+}
